c639a411226f6daab1a65c826ede1b2fdd50f650
Akhil Nair <akhil.nair@amey.co.uk>
2018-07-10 18:13:17 +0100

    Added namespace qualifiers

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -9,7 +9,6 @@ describe("pool package", {
     pool <- dbPool(RSQLite::SQLite(), dbname = ":memory:")
 
     it("can create local SQLite pool", {
-      skip_if_not_installed("dplyr")
       expect_equal(class(pool), c("Pool", "R6"))
       info <- list(
         class = "Pool",
@@ -26,9 +25,11 @@ describe("pool package", {
     })
 
     it("can use dplyr syntax to copy table to DB", {
+
       skip_if_not_installed("dplyr")
+
       checkCounts(pool, free = 1, taken = 0)
-      copy_to(pool, flights, "flights",
+      dplyr::copy_to(pool, flights, "flights",
         temporary = FALSE,
         indexes = list(
           c("year", "month", "day"),
@@ -38,13 +39,13 @@ describe("pool package", {
         )
       )
       checkCounts(pool, free = 1, taken = 0)
-      expect_true(db_has_table(pool, "flights"))
+      expect_true(dplyr::db_has_table(pool, "flights"))
     })
 
     it("can use dplyr syntax to get a table from DB", {
       skip_if_not_installed("dplyr")
       checkCounts(pool, free = 1, taken = 0)
-      flights_db <- tbl(pool, "flights")
+      flights_db <- dplyr::tbl(pool, "flights")
       checkCounts(pool, free = 1, taken = 0)
       expect_equal(class(flights_db), c("tbl_dbi", "tbl_sql", "tbl_lazy", "tbl"))
     })
@@ -52,7 +53,7 @@ describe("pool package", {
     it("can use dplyr syntax to select", {
       skip_if_not_installed("dplyr")
       checkCounts(pool, free = 1, taken = 0)
-      flights_db <- tbl(pool, "flights")
+      flights_db <- dplyr::tbl(pool, "flights")
       s <- dplyr::select(flights_db, year:day, dep_delay, arr_delay)
       expect_equal(tibble::as_tibble(s),
         tibble::tibble(
@@ -69,7 +70,7 @@ describe("pool package", {
     it("can use dplyr syntax to filter", {
       skip_if_not_installed("dplyr")
       checkCounts(pool, free = 1, taken = 0)
-      flights_db <- tbl(pool, "flights")
+      flights_db <- dplyr::tbl(pool, "flights")
       f <- dplyr::filter(flights_db, dep_delay > 0)
       ft <- tibble::as_tibble(f)
       expect_equal(ft$dep_time, c(517, 533, 542))
@@ -80,7 +81,7 @@ describe("pool package", {
     it("can use dplyr syntax to `collect`", {
       skip_if_not_installed("dplyr")
       checkCounts(pool, free = 1, taken = 0)
-      flights_db <- tbl(pool, "flights")
+      flights_db <- dplyr::tbl(pool, "flights")
       c <- dplyr::collect(flights_db)
       expect_equal(c, flights)
       expect_equal(nrow(c), 10)
@@ -89,7 +90,7 @@ describe("pool package", {
 
     it("throws error when `temporary = TRUE`", {
       skip_if_not_installed("dplyr")
-      expect_error(copy_to(pool, flights, "temp"),
+      expect_error(dplyr::copy_to(pool, flights, "temp"),
         "You cannot use `temporary = TRUE`"
       )
     })
